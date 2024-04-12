350884d1359b0492829dcf1c9b4dc7a5e4095ce9
Omobayode6 <omobayode93@gmail.com>
2024-04-12 22:30:55 +0100

    Make changes to contents

--- src/pages/PaBusiness.jsx
+++ src/pages/PaBusiness.jsx
@@ -6,6 +6,7 @@ import AccordionFaq from '../components/AccordionFaq'
 import Accordion from '../components/Accordion'
 import { Link } from 'react-router-dom'
 import pa from '../assets/images/employment-consult.png'
+import Testimonials from '../components/Testimonials'
 
 const PaBusiness = () => {
   return (
@@ -102,38 +103,7 @@ const PaBusiness = () => {
         </div>
 
         {/* Testimonial */}
-        <div className='px-[5%] bg-[#F9FAFB] py-[50px] flex flex-col md:flex-row justify-center items-center gap-x-12'>
-          <div >
-            <p className='text-[14px] font-medium mb-2'>TESTIMONIALS</p>
-            <h2 className='max-w-[311px] mb-8 leading-tight'>What Our Clients Are Saying About Us</h2>
-          </div>
-
-          <div className='flex flex-col gap-4 mb-4 md:mb-0'>
-            <div className='flex flex-col border border-[#E4D4C1] rounded-[18px] bg-white max-w-[392px] min-h-[183px] p-[16px] md:mb-auto'>
-              <p className='text-[13px] font-light mb-4 '>`Folake&apos;s dedication and expertise in the field of immigration coaching are truly commendable. What sets Fola apart is her personalized approach to coaching. She takes the time to understand her client&apos;s specific circumstances, aspirations, and concerns. I wholeheartedly recommend her as an immigration coach because her professionalism, expertise, and genuine commitment to her clients make her a standout in the field. Fola delivers in a timely manner.`</p>
-              <p className='text-[13px] text-primary font-medium mb-1'>Bukola Amao-Taiwo (Ph.D)</p>
-              <p className='text-[13px] font-light'>Entrepreneurship Research</p>
-            </div>
-            <div className='flex flex-col border border-[#E4D4C1] rounded-[18px] bg-white max-w-[392px] min-h-[183px] p-[16px]'>
-              <p className='text-[13px] font-light mb-4 '>`It was amazing! Fola was super helpful in assisting me negotiate legal settlements. Her guidance and counsel enabled me make better informed decisions.`</p>
-              <p className='text-[13px] text-primary font-medium mb-1'>Daniel Ejairu</p>
-              <p className='text-[13px] font-light'>Supply chain Analyst at Belden Inc.</p>
-            </div>
-          </div>
-
-          <div className='flex flex-col gap-4'>
-            <div className='flex flex-col border border-[#E4D4C1] rounded-[18px] bg-white max-w-[392px] min-h-[183px] p-[16px] md:mb-auto'>
-              <p className='text-[13px] font-light mb-4 '>`Fola&apos;s level of professionalism in reviewing my CV was topnotch and the way she interpreted my roles and was able to communicate same in my CV was very brillant.Thank you Fola`</p>
-              <p className='text-[13px] text-primary font-medium mb-1'>Ola&apos; Aruwayo</p>
-              <p className='text-[13px] font-light'>International Business law</p>
-            </div>
-            <div className='flex flex-col border border-[#E4D4C1] rounded-[18px] bg-white max-w-[392px] min-h-[183px] p-[16px]'>
-              <p className='text-[13px] font-light mb-4 '>`Working with Fola has been an absolute delight. She is a talented professional who is not only knowledgeable in her industry, but also committed to giving excellent service and assistance. She takes her time to understand specific requirements and creates a solution that exceedes expectations. I cannot recommend Fola highly enough for any business/legal seeking counsel, solutions, and great customer service!`</p>
-              <p className='text-[13px] text-primary font-medium mb-1'>Ehi Eseka</p>
-              <p className='text-[13px] font-light'>Project Coordinator</p>
-            </div>
-          </div>
-        </div>
+        <Testimonials/>
         
       </div>
       <Footer/>
