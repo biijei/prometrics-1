9ab5a4c0bb13bc720dcd054b31e9faeaa4e2ba1b
Adebunmi Adewusi <adebunmiadewusi@Mac.lan>
2025-10-25 16:19:31 -0400

    Added pictures to the gallery section

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -6,6 +6,48 @@ import ca12 from '../assets/images/caros12.png'
 import ca13 from '../assets/images/caros13.png'
 import ca14 from '../assets/images/caros14.png'
 import ca15 from '../assets/images/caros15.png'
+import ca16 from '../assets/images/caros16.png'
+import ca17 from '../assets/images/caros17.png'
+import ca18 from '../assets/images/caros18.png'
+import ca19 from '../assets/images/caros19.png'
+import ca20 from '../assets/images/caros20.png'
+import ca21 from '../assets/images/caros21.png'
+import ca22 from '../assets/images/caros22.png'
+import ca23 from '../assets/images/caros23.png'
+import ca24 from '../assets/images/caros24.png'
+import ca25 from '../assets/images/caros25.png'
+import ca26 from '../assets/images/caros26.png'
+import ca27 from '../assets/images/caros27.png'
+import ca28 from '../assets/images/caros28.png'
+import ca29 from '../assets/images/caros29.png'
+import ca30 from '../assets/images/caros30.png'
+import ca31 from '../assets/images/caros31.png'
+import ca32 from '../assets/images/caros32.png'
+import ca33 from '../assets/images/caros33.png'
+import ca34 from '../assets/images/caros34.png'
+import ca35 from '../assets/images/caros35.png'
+import ca36 from '../assets/images/caros36.png'
+import ca37 from '../assets/images/caros37.png'
+import ca38 from '../assets/images/caros38.png'
+import ca39 from '../assets/images/caros39.png'
+import ca40 from '../assets/images/caros40.png'
+import ca41 from '../assets/images/caros41.png'
+import ca42 from '../assets/images/caros42.png'
+import ca43 from '../assets/images/caros43.png'
+import ca44 from '../assets/images/caros44.png'
+import ca45 from '../assets/images/caros45.png'
+import ca46 from '../assets/images/caros46.png'
+import ca47 from '../assets/images/caros47.png'
+import ca48 from '../assets/images/caros48.png'
+import ca49 from '../assets/images/caros49.png'
+import ca50 from '../assets/images/caros50.png'
+import ca51 from '../assets/images/caros51.png'
+import ca52 from '../assets/images/caros52.png'
+import ca53 from '../assets/images/caros53.png'
+import ca54 from '../assets/images/caros54.png'
+import ca55 from '../assets/images/caros55.png'
+import ca56 from '../assets/images/caros56.png'
+import ca57 from '../assets/images/caros57.png'
 import Footer from '../components/Footer';
 
 const Gallery = () => {
@@ -135,10 +177,52 @@ const Gallery = () => {
 export default Gallery;
 
 const images = [
-  { url: ca10, alt: 'Testimonial', category: 'eb1a' },
+  { url: ca30, alt: 'Testimonial', category: 'eb1a' },
+  { url: ca32, alt: 'Testimonial', category: 'eb1a' },
+  { url: ca34, alt: 'Testimonial', category: 'eb1a' },
+  { url: ca26, alt: 'Testimonial', category: 'eb1a' },
+  { url: ca25, alt: 'Testimonial', category: 'eb1a' },
+  { url: ca24, alt: 'Testimonial', category: 'eb1a' },
+  { url: ca22, alt: 'Testimonial', category: 'eb1a' },
+  { url: ca21, alt: 'Testimonial', category: 'eb1a' },
   { url: ca11, alt: 'Testimonial', category: 'eb1a' },
-  { url: ca12, alt: 'Testimonial', category: 'eb2niw' },
-  { url: ca13, alt: 'Testimonial', category: 'h1b' },
-  { url: ca14, alt: 'Testimonial', category: 'status-change' },
+  { url: ca47, alt: 'Testimonial', category: 'eb1a' },
+  { url: ca48, alt: 'Testimonial', category: 'eb1a' },
+  { url: ca52, alt: 'Testimonial', category: 'eb1a' },
+  { url: ca37, alt: 'Testimonial', category: 'eb2niw' },
+  { url: ca27, alt: 'Testimonial', category: 'eb2niw' },
+  { url: ca28, alt: 'Testimonial', category: 'eb2niw' },
+  { url: ca36, alt: 'Testimonial', category: 'eb2niw' },
+  { url: ca10, alt: 'Testimonial', category: 'eb2niw' },
+  { url: ca14, alt: 'Testimonial', category: 'eb2niw' },
+  { url: ca18, alt: 'Testimonial', category: 'eb2niw' },
+  { url: ca19, alt: 'Testimonial', category: 'eb2niw' },
+  { url: ca20, alt: 'Testimonial', category: 'eb2niw' },
+  { url: ca23, alt: 'Testimonial', category: 'eb2niw' },
+  { url: ca42, alt: 'Testimonial', category: 'eb2niw' },
+  { url: ca43, alt: 'Testimonial', category: 'eb2niw' },
+  { url: ca46, alt: 'Testimonial', category: 'eb2niw' },
+  { url: ca49, alt: 'Testimonial', category: 'eb2niw' },
+  { url: ca50, alt: 'Testimonial', category: 'eb2niw' },
+  { url: ca51, alt: 'Testimonial', category: 'eb2niw' },
+  { url: ca53, alt: 'Testimonial', category: 'eb2niw' },
+  { url: ca54, alt: 'Testimonial', category: 'eb2niw' },
+  { url: ca57, alt: 'Testimonial', category: 'eb2niw' },
+  { url: ca31, alt: 'Testimonial', category: 'h1b' },
+  { url: ca35, alt: 'Testimonial', category: 'h1b' },
+  { url: ca17, alt: 'Testimonial', category: 'h1b' },
+  { url: ca41, alt: 'Testimonial', category: 'h1b' },
+  { url: ca44, alt: 'Testimonial', category: 'h1b' },
+  { url: ca45, alt: 'Testimonial', category: 'h1b' },
+  { url: ca38, alt: 'Testimonial', category: 'status-change' },
+  { url: ca16, alt: 'Testimonial', category: 'status-change' },
+  { url: ca12, alt: 'Testimonial', category: 'status-change' },
+  { url: ca40, alt: 'Testimonial', category: 'status-change' },
+  { url: ca56, alt: 'Testimonial', category: 'status-change' },
+  { url: ca29, alt: 'Testimonial', category: 'others' },
+  { url: ca33, alt: 'Testimonial', category: 'others' },
+  { url: ca13, alt: 'Testimonial', category: 'others' },
   { url: ca15, alt: 'Testimonial', category: 'others' },
+  { url: ca39, alt: 'Testimonial', category: 'others' },
+  { url: ca55, alt: 'Testimonial', category: 'others' },
 ];
